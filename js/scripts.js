654b5077df449b38c1a5196a04dfb20461b464a9
kmktu <kmktu@naver.com>
2023-06-06 21:19:03 +0900

    MOD : create introduce

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+function openPop() {
+    document.getElementById("popup_layer").style.display = "block";
+
+}
+
+//팝업 닫기
+function closePop() {
+    document.getElementById("popup_layer").style.display = "none";
+}
